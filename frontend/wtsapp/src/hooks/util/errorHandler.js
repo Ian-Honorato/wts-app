65d9ad06c64a28c22da3a2dd683f7a74d7e477ed
Ian Honorato <ian.honorato@gmail.com>
2025-09-30 19:35:23 -0300

    errohandler.js no frontend - erro de parametro

--- frontend/wtsapp/src/hooks/util/errorHandler.js
+++ frontend/wtsapp/src/hooks/util/errorHandler.js
@@ -1,12 +1,10 @@
 export const extractErrorMessage = (error) => {
-  // Erro de validação estruturado do backend (com o novo padrão)
   if (
     error.response?.data?.details &&
     Array.isArray(error.response.data.details)
   ) {
     return error.response.data.details[0].message;
   }
-
   if (error.response?.data?.error) {
     return error.response.data.error;
   }
